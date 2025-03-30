c141199a5b4bb7c6e20789271cbf3e2fbb13a2a5
P-H-Pancholi <pancholiphalin@outlook.com>
2025-03-30 18:56:37 +0530

    upload thumnail

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,41 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+
+	const maxValue = 10 << 20
+	r.ParseMultipartForm(maxValue)
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error reading thumbnail file", err)
+		return
+	}
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error reading image data", err)
+		return
+	}
+	videoData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error reading metadata", err)
+		return
+	}
+	if videoData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User not owner of video", nil)
+		return
+	}
+	thumbnailData := thumbnail{
+		data:      imageData,
+		mediaType: header.Header.Get("Content-Type"),
+	}
+	videoThumbnails[videoID] = thumbnailData
+	thumbnailUrl := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	videoData.ThumbnailURL = &thumbnailUrl
+	cfg.db.UpdateVideo(videoData)
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	respondWithJSON(w, http.StatusOK, videoData)
+
 }
