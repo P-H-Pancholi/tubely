604412bfff259a9251278ae26f24bdde6925dc11
P-H-Pancholi <pancholiphalin@outlook.com>
2025-04-01 23:27:02 +0530

    save image on file server

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -40,6 +42,7 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusInternalServerError, "Error reading thumbnail file", err)
 		return
 	}
+	defer file.Close()
 	imageData, err := io.ReadAll(file)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Error reading image data", err)
@@ -58,8 +61,32 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		data:      imageData,
 		mediaType: header.Header.Get("Content-Type"),
 	}
-	imageEnc := base64.StdEncoding.EncodeToString(thumbnailData.data)
-	thumbnailUrl := fmt.Sprintf("data:%s;base64,%s", thumbnailData.mediaType, imageEnc)
+	// imageEnc := base64.StdEncoding.EncodeToString(thumbnailData.data)
+	// thumbnailUrl := fmt.Sprintf("data:%s;base64,%s", thumbnailData.mediaType, imageEnc)
+	extention := strings.Split(thumbnailData.mediaType, "/")[1]
+	filePath := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s.%s", videoID, extention))
+	imageFile, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating file", err)
+		return
+	}
+	defer imageFile.Close()
+	// Reset file pointer before copying
+	if seeker, ok := file.(io.Seeker); ok {
+		_, err := seeker.Seek(0, io.SeekStart)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Error resetting file pointer", err)
+			return
+		}
+	}
+	bytesCopied, err := io.Copy(imageFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error writing file", err)
+		return
+	}
+	fmt.Printf("Copied %d bytes successfully!\n", bytesCopied)
+
+	thumbnailUrl := fmt.Sprintf("http://localhost:%s/assets/%s.%s", cfg.port, videoID, extention)
 	videoData.ThumbnailURL = &thumbnailUrl
 	cfg.db.UpdateVideo(videoData)
 
